4cb8ab79b3e9309c9ca8c3a8ed044adcc059e2a2
Weslley Santos <weslleyqi0@gmail.com>
2022-07-30 20:08:27 -0300

    Create Layouts

--- app/src/main/java/com/weslleyqi0/apprepositories/ui/MainActivity.kt
+++ app/src/main/java/com/weslleyqi0/apprepositories/ui/MainActivity.kt
@@ -2,11 +2,36 @@ package com.weslleyqi0.apprepositories.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.weslleyqi0.apprepositories.R
+import com.weslleyqi0.apprepositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu?.findItem(R.id.action_search)?.actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.d("QUERY--->","$query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.d("NEW_TEXT--->","$newText")
+        return false
     }
 }
